fbf00bf2e4afa7d77940867ac2c1f668a7aecce8
Noah Blumenthal <noah@hackerhasid.com>
2014-06-16 22:06:17 -0400

    1

--- src/bargelogger.cpp
+++ src/bargelogger.cpp
@@ -15,18 +15,18 @@ BargeLogger::BargeLogger (NetworkAccessManager *networkAccessManager) {
   connect(m_networkAccessManager, SIGNAL(resourceTimeout(QVariant)), this, SLOT(_resourceTimeout(QVariant)));
 };
 
-void BargeLogger::_resourceRequested(const QVariant& data, QObject *request) {
+void BargeLogger::_resourceRequested(const QVariantMap& data, QObject *request) {
   if (false) { // TODO: add cdn urls or something?
     request.abort();
   }
   cout << "barge resource requested: " + data["url"] + "\n";
 }
-void BargeLogger::_resourceReceived(const QVariant& data) {
+void BargeLogger::_resourceReceived(const QVariantMap& data) {
   cout << "barge resource received: " + data["url"] + "\n";
 }
-void BargeLogger::_resourceError(const QVariant& data) {
+void BargeLogger::_resourceError(const QVariantMap& data) {
   cout << "barge resource error: " + data["url"] + "\n";
 }
-void BargeLogger::_resourceTimeout(const QVariant& data) {
+void BargeLogger::_resourceTimeout(const QVariantMap& data) {
   cout << "barge resource timeout: " + data["url"] + "\n";
 }
