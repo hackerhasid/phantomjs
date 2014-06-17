28c1a4d62da9a16234efd00a04625b574131297d
Noah Blumenthal <noah@hackerhasid.com>
2014-06-16 22:37:50 -0400

    1

--- src/bargelogger.cpp
+++ src/bargelogger.cpp
@@ -1,6 +1,6 @@
 #include "bargelogger.h"
 #include "networkaccessmanager.h"
-#include<iostream>
+//#include<iostream>
 
 #define UNUSED(x) (void)(x)
 
@@ -22,28 +22,20 @@ void BargeLogger::_resourceRequested(const QVariant& data, QObject *request) {
 
   QMap<QString, QVariant> dataMap = data.toMap();
   QString url = dataMap.value("url").toString();
-  cout << "barge resource requested: ";
-  cout << url;
-  cout << "\n";
+  qDebug() << "barge resource requested:" << url << "\n";
 }
 void BargeLogger::_resourceReceived(const QVariant& data) {
   QMap<QString, QVariant> dataMap = data.toMap();
   QString url = dataMap.value("url").toString();
-  cout << "barge resource received: ";
-  cout << url;
-  cout << "\n";
+  qDebug() << "barge resource received:" << url << "\n";
 }
 void BargeLogger::_resourceError(const QVariant& data) {
   QMap<QString, QVariant> dataMap = data.toMap();
   QString url = dataMap.value("url").toString();
-  cout << "barge resource error: ";
-  cout << url;
-  cout << "\n";
+  qDebug() << "barge resource error:" << url << "\n";
 }
 void BargeLogger::_resourceTimeout(const QVariant& data) {
   QMap<QString, QVariant> dataMap = data.toMap();
   QString url = dataMap.value("url").toString();
-  cout << "barge resource timeout: ";
-  cout << data["url"];
-  cout << "\n";
+  qDebug << "barge resource timeout:" << url << "\n";
 }
