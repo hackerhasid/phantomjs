bf15f09afa86665c08caf7862990fec978b2723f
Noah Blumenthal <noah@hackerhasid.com>
2014-06-17 17:54:11 -0400

    defined destructor in .h

--- src/bargelogger.h
+++ src/bargelogger.h
@@ -7,11 +7,14 @@ class BargeLogger : public QObject {
   
 public:
     BargeLogger(NetworkAccessManager *m_networkAccessManager);
+    virtual ~BargeLogger;
+
 private slots:
   void _resourceRequested(const QVariant& data, QObject *);
   void _resourceReceived(const QVariant& data);
   void _resourceError(const QVariant& data);
   void _resourceTimeout(const QVariant& data);
+
 private:
   NetworkAccessManager *m_networkAccessManager;
 };
